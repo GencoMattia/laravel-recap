1c3c68305b2280adf60edde1a845ac11204f81fa
GencoMattia <gencomattia@hotmail.it>
2024-08-19 15:42:58 +0200

    added migrations for Cards and Colors

---
+++ database/migrations/2024_08_19_133536_create_magic_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('magic_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->unique();
+            $table->string("artist");
+            $table->text("art_url");
+            $table->text("description");
+            $table->smallInteger("attack");
+            $table->smallInteger("defence");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('magic_cards');
+    }
+};
